3c178a473fe52109f94370279f29362c98e3bda4
psmyrdek <przemek.smyrdek@gmail.com>
2022-12-08 12:53:07 +0100

    part 1

--- 008/index.js
+++ 008/index.js
@@ -9,41 +9,34 @@ const grid = fs
 
 let visibleTrees = 0;
 
-function visibleFromLeft(height, row, col) {}
+function visibleFromOneSide(height, row, col, grid) {
 
-function visibleFromRight(height, row, col) {}
+  const treeRow = grid[row].split('')
+  const treeCol = grid.map((gridLine) => gridLine[col])
+  const smallerThanHeight = val => val < height;
 
-function visibleFromTheTop(height, row, col, grid) {}
+  const fromLeft = treeRow.filter((_, index) => index < col).every(smallerThanHeight);
+  const fromRight = treeRow.filter((_, index) => index > col).every(smallerThanHeight);
+  const fromTop = treeCol.filter((_, index) => index < row).every(smallerThanHeight);
+  const fromBottom = treeCol.filter((_, index) => index > row).every(smallerThanHeight);
 
-function visibleFromTheBottom(height, row, col, grid) {}
+  return fromLeft || fromRight || fromTop || fromBottom;
+}
 
-grid.forEach((treesInRow, row) => {
-  treesInRow.split('').forEach((height, col) => {
-    if (height === '0') {
-      console.log(`not a tree at ${row}x${col}`);
-      return;
-    }
+grid.forEach((treesInRowString, row) => {
+  const treesInRow = treesInRowString.split('').map((val) => parseInt(val));
+  treesInRow.forEach((height, col) => {
 
-    if (
-      row === 0 ||
-      row === treesInRow.length - 1 ||
-      col === 0 ||
-      col === grid.length - 1
-    ) {
-      console.log(`tree on the edge at ${row}x${col}`);
+    if (row === 0 || row === treesInRow.length - 1 || col === 0 || col === grid.length - 1) {
       visibleTrees += 1;
       return;
     }
 
-    if (
-      visibleFromLeft(height, row, col) ||
-      visibleFromRight(height, row, col) ||
-      visibleFromTheTop(height, row, col, grid) ||
-      visibleFromTheBottom(height, row, col, grid)
-    ) {
-      console.log(`tree visible ${row}x${col}`);
+    if (visibleFromOneSide(height, row, col, grid)) {
       visibleTrees += 1;
       return;
     }
   });
 });
+
+console.log(visibleTrees);
