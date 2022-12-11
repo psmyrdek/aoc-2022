c36b49a675da32fd5325a3a29acd4a7ce338ccad
psmyrdek <przemek.smyrdek@gmail.com>
2022-12-11 21:04:05 +0100

    Day 10 p 1

--- 010/index.js
+++ 010/index.js
@@ -13,37 +13,34 @@ let cycle = 1
 let instruction = null
 let sum = 0
 
-let ram = {}
-
-while (instruction = instructions.shift()) {
-
-
-
-  console.log(`Cycle: ${cycle}; Instruction: ${instruction}`)
-
-  if (instruction.startsWith('addx')) {
-    const value = parseInt(instruction.split(' ')[1])
-    const executeWhen = cycle + 1
-
-    if (ram[executeWhen]) {
-      throw new Error('Malfunction')
-    } else {
-      console.log(`Storing ${value} to execute during ${executeWhen} cycle`)
-      ram[executeWhen] = value
-    }
-  }
-
-  if (ram[cycle]) {
-    console.log(`Modifying register by ${ram[cycle]}`)
-    register += ram[cycle]
-    delete ram[cycle]
-  }
-
+function checkCycle() {
   if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
     sum += cycle * register
   }
+}
 
+function nextTick() {
   cycle += 1
+  checkCycle()
+}
+
+while (instruction = instructions.shift()) {
+
+  if (instruction.startsWith('addx')) {
+    console.log(`addx - Cycle: ${cycle}`)
+    
+    nextTick()
+
+    const value = parseInt(instruction.split(' ')[1])
+    register += value
+
+    nextTick()
+  }
+
+  if (instruction === 'noop') {
+    console.log(`noop - Cycle: ${cycle}`)
+    nextTick()
+  }
 
 }
 
