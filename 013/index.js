bd40a1384a2bc2573b4c6409b3d27ef8b6d298eb
psmyrdek <przemek.smyrdek@gmail.com>
2022-12-27 12:38:14 +0100

    Day 13

--- 013/index.js
+++ 013/index.js
@@ -5,7 +5,8 @@ const packets = fs
   .readFileSync(join(__dirname, './input.txt'), {
     encoding: 'utf-8',
   })
-  .split('\n');
+  .split('\n')
+  .filter((val) => val);
 
 function deserializePacket(packet) {
   const symbols = packet.split('');
@@ -58,86 +59,87 @@ function asArray(item) {
 }
 
 const ORDERS = {
-    RIGHT: true,
-    WRONG: false,
-    UNDEF: -1
-}
+  RIGHT: true,
+  WRONG: false,
+  UNDEF: -1,
+};
 
 function compare(leftVal, rightVal) {
-  
-    const maxLen = leftVal.length > rightVal.length ? leftVal.length : rightVal.length
+  const maxLen = leftVal.length > rightVal.length ? leftVal.length : rightVal.length;
 
-    let result = ORDERS.UNDEF
+  let result = ORDERS.UNDEF;
 
-    for (let i = 0; i < maxLen; i++) {
+  for (let i = 0; i < maxLen; i++) {
+    const lItem = leftVal[i];
+    const rItem = rightVal[i];
 
-        const lItem = leftVal[i]
-        const rItem = rightVal[i]
-
-        console.log(`Comparing: ${JSON.stringify(lItem)} vs ${JSON.stringify(rItem)}`)
-
-        if (!exists(lItem) || !exists(rItem)) {
-            result = !exists(lItem) && exists([rItem]) ? ORDERS.RIGHT : ORDERS.WRONG
-            break;
-        }
-
-        if (lItem.hasOwnProperty('length') && rItem.hasOwnProperty('length')) {
-            result = compare(lItem, rItem)
-            if (result === ORDERS.UNDEF) {
-                continue;
-            } else {
-                return result;
-            }
-        }
-
-        if (lItem.hasOwnProperty('length') && !rItem.hasOwnProperty('length')) {
-            result = compare(lItem, [rItem])
-            if (result === ORDERS.UNDEF) {
-                continue;
-            } else {
-                return result;
-            }
-        }
-
-        if (!lItem.hasOwnProperty('length') && rItem.hasOwnProperty('length')) {
-            result = compare([lItem], rItem)
-            if (result === ORDERS.UNDEF) {
-                continue;
-            } else {
-                return result;
-            }
-        }
-
-        if (lItem === rItem) {
-            continue;
-        } else {
-            return lItem < rItem ? ORDERS.RIGHT : ORDERS.WRONG
-        }
-
-
+    if (!exists(lItem) || !exists(rItem)) {
+      result = !exists(lItem) && exists([rItem]) ? ORDERS.RIGHT : ORDERS.WRONG;
+      break;
     }
 
-    return result
-  
-}
+    if (lItem.hasOwnProperty('length') && rItem.hasOwnProperty('length')) {
+      result = compare(lItem, rItem);
+      if (result === ORDERS.UNDEF) {
+        continue;
+      } else {
+        return result;
+      }
+    }
 
-let pair = 1;
-let sumOfPairs = 0;
-for (let i = 0; i < packets.length; i += 3) {
-  const left = deserializePacket(packets[i].substring(1, packets[i].length - 1));
-  const right = deserializePacket(packets[i + 1].substring(1, packets[i + 1].length - 1));
+    if (lItem.hasOwnProperty('length') && !rItem.hasOwnProperty('length')) {
+      result = compare(lItem, [rItem]);
+      if (result === ORDERS.UNDEF) {
+        continue;
+      } else {
+        return result;
+      }
+    }
 
-  console.log(`Checking pair ${pair}`);
-  if (compare(left, right)) {
-    sumOfPairs += pair;
-    console.log(`Adding: ${pair}`);
-  } else {
-    console.log('Not right order');
+    if (!lItem.hasOwnProperty('length') && rItem.hasOwnProperty('length')) {
+      result = compare([lItem], rItem);
+      if (result === ORDERS.UNDEF) {
+        continue;
+      } else {
+        return result;
+      }
+    }
+
+    if (lItem === rItem) {
+      continue;
+    } else {
+      return lItem < rItem ? ORDERS.RIGHT : ORDERS.WRONG;
+    }
   }
-  console.log('----');
 
-
-  pair += 1;
+  return result;
 }
 
-console.log(sumOfPairs);
+packets.push('[[2]]');
+packets.push('[[6]]');
+
+const sorted = packets
+  .map((p) => deserializePacket(p.substring(1, p.length - 1)))
+  .sort((left, right) => (compare(left, right) ? -1 : 1))
+  .map((val) => JSON.stringify(val));
+
+const fSym = sorted.findIndex((val) => val === '[[2]]') + 1;
+const sSym = sorted.findIndex((val) => val === '[[6]]') + 1;
+console.log(fSym * sSym);
+
+// let pair = 1;
+// let sumOfPairs = 0;
+// for (let i = 0; i < packets.length; i += 3) {
+//   const left = deserializePacket(packets[i].substring(1, packets[i].length - 1));
+//   const right = deserializePacket(packets[i + 1].substring(1, packets[i + 1].length - 1));
+
+//   if (compare(left, right)) {
+//     sumOfPairs += pair;
+//   } else {
+//   }
+//   console.log('----');
+
+//   pair += 1;
+// }
+
+// console.log(sumOfPairs);
